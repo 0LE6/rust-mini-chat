1e73ffeca024d6dabc4d7b6f933b96e4fa3b9286
Oleg <kharenkoleg@gmail.com>
2024-06-16 00:12:50 +0200

    chat unilateral de momento

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use actix::{Actor, Addr, Message};
+use actix::{Actor};
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Error};
 use actix::prelude::*;
 use actix_web_actors::ws;
-use std::collections::HashSet;
 //use tokio::stream;
 
 // Esto fue una primera prueba de conexión con el servidor
@@ -12,35 +11,11 @@ use std::collections::HashSet;
 
 
 struct WebSocket {
-    sessions: HashSet<Addr<WebSocket>>,
 }
-
-impl WebSocket {
-    pub fn new() -> Self {
-        WebSocket {
-            sessions: HashSet::new(),
-        }
-    }
-}
-
-#[derive(Message)]
-#[rtype(result = "()")]
-struct MyWebSocketMessage(String);
 
 // En Actix, un actor es un objeto que encapsula el estado y el comportamiento. Los actores comunican entre sí exclusivamente por mensajes.
 impl Actor for WebSocket {
     type Context = ws::WebsocketContext<Self>;
-
-    // Método que se llama cuando el actor se crea.
-    fn started(&mut self, ctx: &mut Self::Context) {
-        self.sessions.insert(ctx.address());
-    }
-
-    // Método que se llama cuando el actor se detiene.
-    fn stopping(&mut self, ctx: &mut Self::Context) -> Running {
-        self.sessions.remove(&ctx.address());
-        Running::Stop
-    }
 }
 
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocket {
@@ -56,12 +31,6 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocket {
             },
             Ok(ws::Message::Text(text)) => {
                 println!("Mensaje de texto recibido: {:?}", text);
-                // Retransmitir el mensaje a todas las sesiones
-                for session in &self.sessions {
-                    session.do_send(MyWebSocketMessage(text.clone())).unwrap_or_else(|e| {
-                        println!("Error al enviar mensaje: {:?}", e);
-                    });
-                }
             },
             Ok(ws::Message::Binary(bin)) => {
                 println!("Datos binarios recibidos");
@@ -77,8 +46,8 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocket {
 
 // Ruta para iniciar la conexión WebSocket
 async fn ruta_del_chat(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-   let websocket = WebSocket { sessions: HashSet::new() }; // Inicializa con sessions
-    ws::start(websocket, &req, stream)}
+    ws::start(WebSocket {}, &req, stream)
+}
 
 
 #[actix_web::main] // atributo para iniciar el sistema Actix
